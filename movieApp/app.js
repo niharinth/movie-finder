0f1588211de24cae47b0749a72a06c1ccf0b120c
Niharinth S Selva <47297903+niharinth@users.noreply.github.com>
2019-04-18 01:17:37 +0530

    Add files via upload

---
+++ movieApp/app.js
@@ -0,0 +1,27 @@
+var express = require("express");
+var request = require("request");
+var app = express();
+
+app.set("view engine", "ejs");
+
+app.get("/", function(req, res) {
+  res.render("search");
+});
+
+app.get("/results", function(req, res) {
+  var query = req.query.search;
+  request("http://www.omdbapi.com/?s=" + query + "&apikey=7a71472d", function(
+    error,
+    response,
+    body
+  ) {
+    if (!error && response.statusCode == 200) {
+      var data = JSON.parse(body);
+      res.render("results", { data: data });
+    }
+  });
+});
+
+app.listen(3000, function() {
+  console.log("server is on");
+});
